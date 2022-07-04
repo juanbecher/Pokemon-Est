ba858a70c064a959790832a2318a38e81f6eb5ba
juanbecher <juanbecher1895@gmail.com>
2022-07-04 15:38:35 +1000

    Building general structure and adding trpc

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    body{
+        color: white;
+        background-color: lightsalmon;
+        max-width:1200px;
+        width:95%;
+        margin: 0 auto;
+    }
+`
