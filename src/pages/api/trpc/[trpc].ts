88823f8eb758180627fe35a623832ad75b9d9de9
juanbecher <juanbecher1895@gmail.com>
2022-07-05 19:49:58 +1000

    Making vote persistant with prisma and planetscale

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,6 +2,8 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 import { PokemonClient } from 'pokenode-ts';
+import { inferProcedureOutput } from "@trpc/server";
+import {prisma} from '../../../utils/prisma'
 
 
 export const appRouter = trpc
@@ -14,8 +16,22 @@ export const appRouter = trpc
     async resolve({ input }) {
       const api = new PokemonClient();
       const pokemon = await api.getPokemonById(input.id)
-      return {name: pokemon.name, sprites: pokemon.sprites};
+      return {name: pokemon.name, sprites: pokemon.sprites, id: pokemon.id};
     },
+  }).mutation('vote-for-pokemon', {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number()
+    }),
+    async resolve({input}) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          votedAgainst: input.votedAgainst,
+          votedFor: input.votedFor,
+        },
+      });
+      return {success: true, vote: voteInDb }
+    }
   });
 
 // export type definition of API
@@ -26,3 +42,7 @@ export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 });
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
